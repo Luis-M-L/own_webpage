59089e593dd9dcd1f77ee5f173e053df907959c9
Luis <luis@localhost.localdomain>
2018-06-02 22:25:36 +0200

    Calling setListener for each ul

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function onLoad(){
   console.log("Hola");
+
+
+  $(".sort-down").siblings("ul").each(function(){
+    setListener($(this));
+  } );
+}
+
+function setListener(elem){
+  //console.log("setListener");
+  console.log(elem);
 }
